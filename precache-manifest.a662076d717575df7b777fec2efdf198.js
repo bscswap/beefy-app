78d1b495e72c4f7581b338aeba5c8fa004e6b9e2
Hyunsik Lee <hyunsiklee@bscswap.com>
2020-11-05 18:20:59 +0900

    Initial commit

---
+++ precache-manifest.a662076d717575df7b777fec2efdf198.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d110159f75a01c0d89cbdced3ac4ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c1c70a0ade387622880",
+    "url": "/static/css/main.9ea70a39.chunk.css"
+  },
+  {
+    "revision": "bc6be7b683adf5d838c0",
+    "url": "/static/js/2.3cd759da.chunk.js"
+  },
+  {
+    "revision": "2c1c70a0ade387622880",
+    "url": "/static/js/main.81dfd173.chunk.js"
+  },
+  {
+    "revision": "a1d1ff2ccae6b2eb0cd4",
+    "url": "/static/js/runtime-main.48f7e4d2.js"
+  },
+  {
+    "revision": "bba1cc02264278d46ac1e813be33a3a2",
+    "url": "/static/media/BURGER.bba1cc02.png"
+  },
+  {
+    "revision": "d569caeed9945646295904122bd90f2c",
+    "url": "/static/media/BUSD.d569caee.png"
+  },
+  {
+    "revision": "f6fd018ec5b05627c00ce5adfdbea187",
+    "url": "/static/media/BUSD.f6fd018e.png"
+  },
+  {
+    "revision": "37e4f26b52b7f42f079da7cd30eeaf96",
+    "url": "/static/media/ETH.37e4f26b.png"
+  },
+  {
+    "revision": "f66b0cf3ab573715ce7fadb693f9702b",
+    "url": "/static/media/LINK.f66b0cf3.png"
+  },
+  {
+    "revision": "936da5e6733580b0dad4dd193042ab17",
+    "url": "/static/media/LOADEFI.936da5e6.png"
+  },
+  {
+    "revision": "4d06ed03ee9590d36fd7f81e10d97093",
+    "url": "/static/media/USDT.4d06ed03.png"
+  },
+  {
+    "revision": "fc0a2d00d61d490a87571dd384c85994",
+    "url": "/static/media/WBNB.fc0a2d00.png"
+  },
+  {
+    "revision": "5946a56bd7c7888f07a6f26f6e31e07d",
+    "url": "/static/media/metamask.5946a56b.png"
+  }
+]);
